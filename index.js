cfc20ae0237a047cee5f40bc8ee6e3a9bec310d3
dementevmikhail91-jpg <dementevmikhail91@gmail.com>
2026-04-25 21:03:33 +0300

    Enhance user connection logging and room management

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  console.log("User connected");
+  console.log("User connected:", socket.id);
 
+  // вход в комнату (фикс!)
   socket.on("join", (room) => {
+    // выйти из всех комнат кроме своей
+    Object.keys(socket.rooms).forEach((r) => {
+      if (r !== socket.id) socket.leave(r);
+    });
+
     socket.join(room);
+    console.log(`User ${socket.id} joined ${room}`);
   });
 
+  // сообщение
   socket.on("message", (data) => {
     io.to(data.room).emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
